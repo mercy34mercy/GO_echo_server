c23a353524163906f81d49eb179b3ec64ac3994e
makun <etukobamasatyan@gmail.com>
2021-12-26 19:53:57 +0900

    linux→Win環境に移行

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 	"github.com/labstack/echo/v4"
+	"io/ioutil"
 )
 
 // この関数を追加
@@ -12,7 +13,7 @@ func port() string {
 	port := os.Getenv("PORT")
 
 	if len(port) == 0 {
-		port = "8080"
+		port = "1234"
 	}
 
 	return ":" + port
@@ -20,9 +21,21 @@ func port() string {
 
 
 func main() {
+
+	url := "https://quiet-stream-64429.herokuapp.com/url"
+	
+
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		req, _ := http.NewRequest("GET", url, nil)
+		client := new(http.Client)
+		resp, _ := client.Do(req)
+		defer resp.Body.Close()
+  
+		byteArray, _ := ioutil.ReadAll(resp.Body)
+		//fmt.Println(string(byteArray))
+		return c.String(http.StatusOK, string(byteArray))
 	})
 	// Port番号を関数から取得
 	e.Logger.Fatal(e.Start(port()))
